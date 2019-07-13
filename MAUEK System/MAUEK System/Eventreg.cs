edb64e6ce6c541ec3cc72478abb273b24e2fa463
TeaRX27 <cursedx126@gmail.com>
2019-07-13 20:40:33 +0800

    > Added Event Registration User Control

---
+++ MAUEK System/MAUEK System/Eventreg.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MAUEK_System
+{
+    public partial class Eventreg : UserControl
+    {
+        public Eventreg()
+        {
+            InitializeComponent();
+        }
+
+        private void groupBox2_Enter(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
